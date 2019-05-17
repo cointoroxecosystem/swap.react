12f15a42bd24140c1b1b6162aaf86921c424fd7a
Leon Song <49976250+leonatcointorox@users.noreply.github.com>
2019-05-17 19:45:57 +1000

    Update index.js

--- shared/components/ui/CurrencyIcon/images/index.js
+++ shared/components/ui/CurrencyIcon/images/index.js
@@ -1,42 +1,39 @@
-import btc from './btc.png'
-import eth from './eth.png'
+import btc from './btc.svg'
+import eth from './eth.svg'
 import icx from './icx.svg'
 import waves from './waves.svg'
 import xrp from './xrp.svg'
 import xlm from './xlm.svg'
 // import bch from './bch.svg'
 import nim from './nim.svg'
-import eos from './eos.png'
-import tlos from './telos.png'
-import swap from './swap.png'
-import usdt from './usdt.png'
+import eos from './eos.svg'
+import swap from './swap.svg'
+import usdt from './usdt.svg'
 import syc2 from './syc2.svg'
 import drt from './drt.svg'
-import omg from './omg.png'
 import yup from './yup.svg'
 import kn from './kn.svg'
 import arn from './arn.svg'
 import lev from './lev.svg'
-import ltc from './ltc.png'
+import ltc from './ltc.svg'
 import bxb from './bxb.svg'
 import dcn from './dcn.svg'
-import mkr from './mkr.png'
+
+import mkr from './mkr.svg'
 import rep from './rep.svg'
 import zrx from './zrx.svg'
 import zil from './zil.svg'
-import gnt from './gnt.png'
+import golem from './gnt.png'
+
 import bat from './bat.png'
+import omg from './omg.png'
 import hot from './hot.png'
 import link from './link.png'
 import orox from './orox.png'
 import usdc from './usdc.png'
 import tusd from './tusd.png'
 import dai from './dai.svg'
-import gusd from './gusd.png'
-import bnty from './bnty.png'
 import mth from './mth.png'
-
-
 
 
 export default {
@@ -65,18 +62,15 @@ export default {
   mkr,
   bat,
   rep,
-  omisego,
   hot,
   link,
   zrx,
   zil,
-  gnt,
+  golem,
   orox,
-  gusd,
-  mth,
-  bnty,
 
   usdc,
   tusd,
   dai,
+  mth,
 }
