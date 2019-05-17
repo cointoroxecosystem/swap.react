706a7994c666175993c97304863e41c85e8ee0c4
Leon Song <49976250+leonatcointorox@users.noreply.github.com>
2019-05-17 13:12:11 +1000

    Update tips text

--- shared/helpers/tips.js
+++ shared/helpers/tips.js
@@ -1,8 +1,8 @@
 const tips = {
   loader: [
     'Do not forget to save your private keys!',
-    '"Wow, this is awesome!" – @dexx7y Maintainer of Omni Core about our USDT swaps',
-    '"That is great news" – Roger Ver, CEO of bitcoin.com, about connecting Bitcoin Cash to Atomicswapwallet.io',
+    '100% Decentralized Cryptocurrency Wallet Supporting p2p Coin & Token Swaps',
+    'Atomicswapwallet.io Does Not Store Your Keys, Coins, and Data. Remember to Save Your Private Keys!',
   ],
 }
 
