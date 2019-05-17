385cf6ae029722db3d6e12aaedd1a6796628ae28
Leon Song <49976250+leonatcointorox@users.noreply.github.com>
2019-05-17 19:48:53 +1000

    Update index.js

--- shared/components/ui/CurrencyIcon/images/index.js
+++ shared/components/ui/CurrencyIcon/images/index.js
@@ -1,14 +1,14 @@
-import btc from './btc.svg'
-import eth from './eth.svg'
+import btc from './btc.png'
+import eth from './eth.png'
 import icx from './icx.svg'
 import waves from './waves.svg'
 import xrp from './xrp.svg'
 import xlm from './xlm.svg'
 // import bch from './bch.svg'
 import nim from './nim.svg'
-import eos from './eos.svg'
-import swap from './swap.svg'
-import usdt from './usdt.svg'
+import eos from './eos.png'
+import swap from './swap.png'
+import usdt from './usdt.png'
 import syc2 from './syc2.svg'
 import drt from './drt.svg'
 import yup from './yup.svg'
@@ -19,7 +19,7 @@ import ltc from './ltc.svg'
 import bxb from './bxb.svg'
 import dcn from './dcn.svg'
 
-import mkr from './mkr.svg'
+import mkr from './mkr.png'
 import rep from './rep.svg'
 import zrx from './zrx.svg'
 import zil from './zil.svg'
@@ -34,6 +34,7 @@ import usdc from './usdc.png'
 import tusd from './tusd.png'
 import dai from './dai.svg'
 import mth from './mth.png'
+import gusd from './gusd.png'
 
 
 export default {
@@ -73,4 +74,5 @@ export default {
   tusd,
   dai,
   mth,
+  gusd,
 }
