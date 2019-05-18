c38f2d5eabc65ab20a272a6a694080a33da7eefb
Leon Song <49976250+leonatcointorox@users.noreply.github.com>
2019-05-18 17:03:57 +1000

    Update About.js

--- shared/pages/About/About.js
+++ shared/pages/About/About.js
@@ -8,7 +8,24 @@ import { FormattedMessage } from 'react-intl'
 
 const About = ({ address }) => (
   <section style={{ height: '100%' }}>
-    <iframe width="100%" height="750px" style={{ marginTop: "30px" }} title="About AtomicSwapWallet.io" src={` https://medium.com/@cointorox/atomicswapwallet-io-your-coins-your-keys-cf2530bceb0/`} frameBorder="0" />
+  <h1>AtomicSwapWallet.io</h1>
+  <br><br><h5>100% Decentralized Browser-based Cryptocurrency Wallet Supporting Atomic Swaps</h5>
+  <br><br><b>Supported Coins for Wallet:</b> Bitcoin, Bitcoin Cash, EOS, Telos (coming soon), Litecoin, USDT(omni), Ethereum, and various ERC20 Tokens
+  <br><br><b>Supported Pairs for Atomic Swaps:</b> 
+  <br>
+  <li>Swaps for BCH, EOS, TLOS are coming soon</li>
+  <li>Bitcoin can be Swapped for Ethereum, ERC20 Tokens, Litecoin</li>
+  <li>Ethereum can be Swapped for Bitcoin, Litecoin</li>
+  <li>Litecoin can be Swapped for Bitcoin, Ethereum</li>
+  <li>USDT can be Swapped for only ERC20 Tokens</li>
+  <li>ERC20 Tokens can be Swapped for Bitcoin or USDT</li>
+  <br><br>
+  <h4>What are Atomic Swaps? Explained in Simple English</h4>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/fh-i8lVhN9o" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <br><br>
+  <h4>Atomic Swaps Explained in 3 Mins</h4>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/WkXUz3UFn6Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <br><br>
   </section>
 )
 
