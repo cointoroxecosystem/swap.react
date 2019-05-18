2dcf3102cab40b80cd888676dc559d94306b023a
Leon Song <49976250+leonatcointorox@users.noreply.github.com>
2019-05-18 19:26:45 +1000

    Update erc20 list

--- config/mainnet/erc20.js
+++ config/mainnet/erc20.js
@@ -1,83 +1,102 @@
 export default {
-  orox: {
+orox: {
     address: '0x1c5b760f133220855340003b43cc9113ec494823',
     decimals: 18,
     fullName: 'Cointorox',
   },
-  gusd: {
-    address: '0x056fd409e1d7a124bd7017459dfea2f387b6d5cd',
-    decimals: 2,
-    fullName: 'Gemini dollar',
-  },
-  dai: {
-    address: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
-    decimals: 18,
-    fullName: 'Dai StableCoin',
-  },
-  usdc: {
-    address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-    decimals: 6,
-    fullName: 'USDC',
-  },
-  tusd: {
-    address: '0x0000000000085d4780B73119b644AE5ecd22b376',
-    decimals: 18,
-    fullName: 'TrueUSD',
-  },
-  mth: {
-    address: '0xaf4dce16da2877f8c9e00544c93b62ac40631f16',
-    decimals: 5,
-    fullName: 'Monetha',
-  },
-  mkr: {
-    address: '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2',
-    decimals: 18,
-    fullName: 'Maker',
-  },
-  bat: {
+bat: {
     address: '0x0d8775f648430679a709e98d2b0cb6250d2887ef',
     decimals: 18,
     fullName: 'Basic Attention Token',
   },
-  rep: {
-    address: '0x1985365e9f78359a9B6AD760e32412f4a445E862',
+bnt: {
+    address: '0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c',
     decimals: 18,
-    fullName: 'Augur',
+    fullName: 'Bancor Network Token',
   },
-  omg: {
-    address: '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07',
+dai: {
+    address: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
     decimals: 18,
-    fullName: 'OmiseGo',
+    fullName: 'Dai StableCoin',
   },
-  hot: {
+dent: {
+    address: '0x3597bfd533a99c9aa083587b074434e61eb0a258',
+    decimals: 8,
+    fullName: 'Dent',
+  },  
+ext: {
+    address: '0xaf3b6cf6b25740af51cfebcaad5bae2ca12856f0',
+    decimals: 18,
+    fullName: 'ExoToken',
+  },  
+gnt: {
+    address: '0xa74476443119A942dE498590Fe1f2454d7D4aC0d',
+    decimals: 18,
+    fullName: 'Golem',
+  }, 
+gusd: {
+    address: '0x056fd409e1d7a124bd7017459dfea2f387b6d5cd',
+    decimals: 2,
+    fullName: 'Gemini dollar',
+  },
+hot: {
     address: '0x6c6ee5e31d828de241282b9606c8e98ea48526e2',
     decimals: 18,
     fullName: 'Holo',
   },
-  link: {
+link: {
     address: '0x514910771af9ca656af840dff83e8264ecf986ca',
     decimals: 18,
     fullName: 'Chainlink',
   },
-  zrx: {
-    address: '0xe41d2489571d322189246dafa5ebde1f4699f498',
-    decimals: 18,
-    fullName: '0x',
+mth: {
+    address: '0xaf4dce16da2877f8c9e00544c93b62ac40631f16',
+    decimals: 5,
+    fullName: 'Monetha',
   },
-  zil: {
+mkr: {
+    address: '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2',
+    decimals: 18,
+    fullName: 'Maker',
+  },
+omg: {
+    address: '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07',
+    decimals: 18,
+    fullName: 'OmiseGo',
+  },
+rep: {
+    address: '0x1985365e9f78359a9B6AD760e32412f4a445E862',
+    decimals: 18,
+    fullName: 'Augur',
+  },
+snt: {
+    address: '0x744d70fdbe2ba4cf95131626614a1763df805b9e',
+    decimals: 18,
+    fullName: 'Status Network',
+  },    
+swap: {
+    address: '0x14a52cf6B4F68431bd5D9524E4fcD6F41ce4ADe9',
+    decimals: 18,
+    fullName: 'swap',
+  },  
+tusd: {
+    address: '0x0000000000085d4780B73119b644AE5ecd22b376',
+    decimals: 18,
+    fullName: 'TrueUSD',
+  },  
+usdc: {
+    address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    decimals: 6,
+    fullName: 'USDC',
+  },
+zil: {
     address: '0x05f4a42e251f2d52b8ed15e9fedaacfcef1fad27',
     decimals: 18,
     fullName: 'Zilliqa',
   },
-  gnt: {
-    address: '0xa74476443119A942dE498590Fe1f2454d7D4aC0d',
+zrx: {
+    address: '0xe41d2489571d322189246dafa5ebde1f4699f498',
     decimals: 18,
-    fullName: 'Golem',
+    fullName: '0x',
   },
-    ext: {
-    address: '0xaf3b6cf6b25740af51cfebcaad5bae2ca12856f0',
-    decimals: 18,
-    fullName: 'ExoToken',
-  },
-
 }
