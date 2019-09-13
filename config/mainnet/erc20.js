8b0f10896dd68665f903eef2c911a7b19c352dcd
HYPE Token <52849174+HypeToken@users.noreply.github.com>
2019-09-13 20:42:06 +1000

    remove orox

--- config/mainnet/erc20.js
+++ config/mainnet/erc20.js
@@ -74,11 +74,6 @@ omg: {
     decimals: 18,
     fullName: 'OmiseGo',
   },
-orox: {
-    address: '0x1c5b760f133220855340003b43cc9113ec494823',
-    decimals: 18,
-    fullName: 'Cointorox',
-  },    
 rep: {
     address: '0x1985365e9f78359a9B6AD760e32412f4a445E862',
     decimals: 18,
